bedcb9a68454fceca664a15dc340583c3eb6da5a
Alessandro Nakamuta <warthog@jera.com.br>
2016-07-28 11:34:59 -0400

    0.1.0

---
+++ Subspecs/0.1.0/KondutoSDK.framework/Headers/KondutoSDK.h
@@ -0,0 +1,19 @@
+//
+//  KondutoSDK.h
+//  KondutoSDK
+//
+//  Created by Alessandro Nakamuta on 7/26/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KondutoSDK.
+FOUNDATION_EXPORT double KondutoSDKVersionNumber;
+
+//! Project version string for KondutoSDK.
+FOUNDATION_EXPORT const unsigned char KondutoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KondutoSDK/PublicHeader.h>
+
+
